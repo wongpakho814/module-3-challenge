44fcce69d35b9eb924c0c3868fdded0b62143906
wongpakho814 <wongpakho814@gmail.com>
2022-08-29 16:15:49 +1000

    Added length criteria prompt and check

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,34 @@
 // Assignment code here
+var criteria = {
+  isUpper: false,
+  isLower: false,
+  isSpecial: false,
+  isNumber: false,
+  length: 0
+};
 
+// Initiate the process of prompting password criteria from user
+function generatePassword() {
+  let length = prompt("Please select a length from 8 to 128 characters for your password!");
+  checkLength(length);
+
+}
+
+// Get the required length from user, check the input and change the corresponding field in var criteria
+function checkLength(length) {
+  // Check if the user clicked the cancel button
+  if (length == null) { 
+    alert("Please click the generate button to re-enter the criteria").close();
+  }
+  // Check the input is valid
+  else if (isNaN(parseInt(length)) || length < 8 || length > 128) { 
+    checkLength(prompt("Your length is illegal, please try again (choose between 8-128)"));
+  }
+  // If input is valid, update the length field in var criteria
+  else {
+    criteria.length = length;
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +39,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
